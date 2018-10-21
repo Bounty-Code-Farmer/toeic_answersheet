289e213069b8885273f1922045ea2f87e8fa4037
Maxence Detante <maxence.detante@gmail.com>
2018-10-21 21:55:39 +0200

    Add saving mechanism

--- app/src/main/java/com/example/maxence/toeic_answer_sheet/SaveActivity.java
+++ app/src/main/java/com/example/maxence/toeic_answer_sheet/SaveActivity.java
@@ -3,9 +3,20 @@ package com.example.maxence.toeic_answer_sheet;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class SaveActivity extends AppCompatActivity {
+
+    public String answers;
+    //public boolean checked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,9 +24,43 @@ public class SaveActivity extends AppCompatActivity {
         setContentView(R.layout.activity_save);
 
         Intent intent = getIntent();
-        String answers = intent.getStringExtra(AnswerActivity.ANSWERS);
-        TextView data = (TextView) findViewById(R.id.save_data);
-        data.setText(answers);
+        answers = intent.getStringExtra(AnswerActivity.ANSWERS);
+        //checked = intent.getBooleanExtra(CheckActivity.???)
+
+        displayFiles();
 
     }
+
+    public void displayFiles() {
+        TextView files = (TextView) findViewById(R.id.saved_files);
+        String names = "";
+        for(String s : fileList()){
+            names += s + "\n";
+        }
+        files.setText(names);
+    }
+
+    public void save(View view){
+        FileOutputStream output = null;
+        String fileName = "Default";
+
+        EditText editText = (EditText) findViewById(R.id.save_fileName);
+        String editTextContent = editText.getText().toString();
+        if(!editTextContent.equals("") && !editTextContent.contains("txt"))
+            fileName = editText.getText().toString();
+
+        try {
+            output = openFileOutput("TOEIC_" + fileName + ".txt", MODE_PRIVATE);
+            output.write(answers.getBytes());
+            if(output != null)
+                output.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        displayFiles();
+    }
+
 }
