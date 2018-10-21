9526dfae89a440bd1c1c4aab699857d30d5f8375
Maxence Detante <maxence.detante@gmail.com>
2018-10-21 14:36:46 +0200

    Add Answer Activity and link with Home

--- app/src/main/java/com/example/maxence/toeic_answer_sheet/Home.java
+++ app/src/main/java/com/example/maxence/toeic_answer_sheet/Home.java
@@ -1,7 +1,10 @@
 package com.example.maxence.toeic_answer_sheet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void switchToAnswer(View view) {
+        Intent intent = new Intent(this, Answer.class);
+        startActivity(intent);
     }
 }
