52acca77aac77533da980df6ce89e91e9734e315
Maxence Detante <maxence.detante@gmail.com>
2018-10-22 10:51:40 +0200

    Add confirmation message when leaving answer sheet

--- app/src/main/java/com/example/maxence/toeic_answer_sheet/AnswerActivity.java
+++ app/src/main/java/com/example/maxence/toeic_answer_sheet/AnswerActivity.java
@@ -1,6 +1,8 @@
 package com.example.maxence.toeic_answer_sheet;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -30,6 +32,24 @@ public class AnswerActivity extends AppCompatActivity {
         createAnswerLines(mainLayout);
 
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Closing answer sheet")
+                .setMessage("Are you sure you want to close this answer sheet?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 
     public void switchToSave(View view){
